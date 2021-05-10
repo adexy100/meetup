4e739e90b32492cfbdbeda2f9de6bc1f3fbb431e
unknown <adexy100@gmail.com>
2021-05-10 00:03:18 -1200

    setting files

---
+++ frontend/src/components/hoc/withAuth.ts
@@ -0,0 +1,20 @@
+import { useSelector } from "react-redux";
+import { IRootReducer } from "~/types/type";
+
+interface IInjectedProps {
+  isAuth: boolean;
+}
+
+const withAuth = <P extends IInjectedProps>(
+  Component: React.ComponentType<P>
+) => {
+  return (props: Pick<P, Exclude<keyof P, keyof IInjectedProps>>) => {
+    const isAuth = useSelector(
+      (state: IRootReducer) => !!state.auth.id && !!state.auth.username
+    );
+
+    return <Component {...(props as P)} isAuth={isAuth} />;
+  };
+};
+
+export default withAuth;
