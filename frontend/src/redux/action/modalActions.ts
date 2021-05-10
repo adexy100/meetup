c55a8a6f90ac233ae4ecf88274d0b336427f38b7
unknown <adexy100@gmail.com>
2021-05-10 03:23:18 -1200

    setting store

---
+++ frontend/src/redux/action/modalActions.ts
@@ -0,0 +1,18 @@
+import { HIDE_MODAL, SHOW_MODAL } from "~/constants/actionType";
+import { EModalType } from "~/types/type";
+
+export const showModal = (modalType: EModalType) =>
+  <const>{
+    type: SHOW_MODAL,
+    payload: modalType,
+  };
+
+export const hideModal = (modalType: EModalType) =>
+  <const>{
+    type: HIDE_MODAL,
+    payload: modalType,
+  };
+
+export type modalActionType =
+  | ReturnType<typeof showModal>
+  | ReturnType<typeof hideModal>;
